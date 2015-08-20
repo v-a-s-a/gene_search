b5665034751d331b4c6369cb22944ab6b16a5fa6
vasya <trubetskoy.vasa@gmail.com>
2015-08-20 15:44:53 -0400

    Changed type ahead to match on leading characters, and to display shortest matches first.

--- js/app.js
+++ js/app.js
@@ -9,4 +9,10 @@ app.controller('geneSearchController', function($scope, $http) {
     $scope.geneData = data;
   });
 
+  // match on leading characters
+  // http://stackoverflow.com/questions/18429967/angularjs-ui-typeahead-match-on-leading-characters
+  $scope.startsWith = function(gene, viewValue) {
+      return gene.substr(0, viewValue.length).toLowerCase() == viewValue.toLowerCase();
+  } 
+
 });
