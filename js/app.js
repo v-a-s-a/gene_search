edd0a802a25d6affb3a23e6d686ae2ab81cc010f
vasya <trubetskoy.vasa@gmail.com>
2015-08-21 11:13:12 -0400

    Added routing.

--- js/app.js
+++ js/app.js
@@ -1,18 +1,24 @@
 'use strict';
 
-var app = angular.module('geneAdventure', ['ui.bootstrap']);
+var geneAdventure = angular.module('geneAdventure', [
+    'ui.bootstrap',
+    'ngRoute',
+    'geneAdventureControllers',
+    'geneAdventureServices'
+    ]);
 
-app.controller('geneSearchController', function($scope, $http) {
-  // load the gene data
-  $http.get('data/gencode.v19.genePred.hg19.json').success(function(data) {
-    $scope.genes = Object.keys(data);
-    $scope.geneData = data;
-  });
-
-  // match on leading characters
-  // http://stackoverflow.com/questions/18429967/angularjs-ui-typeahead-match-on-leading-characters
-  $scope.startsWith = function(gene, viewValue) {
-      return gene.substr(0, viewValue.length).toLowerCase() == viewValue.toLowerCase();
-  } 
-
-});
+geneAdventure.config(['$routeProvider',
+  function($routeProvider) {
+    $routeProvider.
+      when('/search', {
+        templateUrl: 'partials/search-box.html',
+        controller: 'geneSearchController'
+      }).
+      when('/genes/:geneSymbol', {
+        templateUrl: 'partials/gene-detail.html',
+        controller: 'geneDisplayController'
+      }).
+      otherwise({
+        redirectTo: '/search'
+      });
+  }]);
