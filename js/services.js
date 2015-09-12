ecff17a700b0321ff8027656ab5240cb422cd51e
vasya <trubetskoy.vasa@gmail.com>
2015-09-12 10:04:04 -0400

    deleted the gene data loading service. requests are now made upon keystrokes

--- js/services.js
+++ js/services.js
@@ -2,13 +2,3 @@
 
 var geneAdventureServices = angular.module('geneAdventureServices', []);
 
-geneAdventureServices.service('regionData', ['$http', '$q', function($http, $q) {
-    var deferedRegionData = $q.defer();
-    this.searchRegions = function(startswith) {
-      var regionInfo = $http.get('http://localhost:9000/v1/regionSearch?startswith='+startswith).success(function(data){
-          deferedRegionData.resolve(data);
-        });
-      return deferedRegionData.promise;
-      };
-    this.regionInfo = this.searchRegions().then(function(data){return data});
-    }]);
